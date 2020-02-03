e8fe484cdf94bfccb9b6250dc238ed6866d3b358
Tanya <52540265+tanya-555@users.noreply.github.com>
2020-02-03 11:39:24 +0530

    Update TaxCalculation.java

--- src/Assignment1/TaxCalculation.java
+++ src/Assignment1/TaxCalculation.java
@@ -2,15 +2,15 @@ package Assignment1;
 
 public class TaxCalculation {
 
-    double taxForRaw(double price) {
+    static double taxForRaw(double price) {
         return 0.125 * price;
     }
 
-    double taxForManufactured(double price) {
+    static double taxForManufactured(double price) {
         return 0.125 * price + (0.02 * (price + 0.125 * price));
     }
 
-    double taxForImported(double price) {
+    static double taxForImported(double price) {
 
         double tax = price * 0.1;
         double surcharge = 0.0;
